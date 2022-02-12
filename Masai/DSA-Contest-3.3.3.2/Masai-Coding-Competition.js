aea24df8f554cc4d3373105ec5055a1e617e084f
naszifnaaz <naszifnaaz@gmail.com>
2022-02-12 23:44:36 +0530

    DSA-Contest-3.3.3.2

--- Masai/DSA-Contest-3.3.3.2/Masai-Coding-Competition.js
+++ Masai/DSA-Contest-3.3.3.2/Masai-Coding-Competition.js
@@ -2,3 +2,67 @@
 https://oj.masaischool.com/contest/2938/problem/01
 */
 
+//Queues for each of the four teams
+let q1 = [];
+let q2 = [];
+let q3 = [];
+let q4 = [];
+
+//A master queue
+let queue = [];
+
+function masaiCodingCompetition(input)
+{
+    if(input[0] == 'E')
+    {
+        if(input[1] == '1')
+            q1.push(+(input[2]));
+            
+        else if(input[1] == '2')
+            q2.push(+(input[2]));
+        
+        else if(input[1] == '3')
+            q3.push(+(input[2]));
+        
+        else
+            q4.push(+(input[2]));
+            
+        
+        let group = +(input[1]);
+
+        if(queue.includes(group) == false)
+            queue.push(group);
+    }
+    
+    else
+    {
+        if(queue[0] == 1)
+        {
+            console.log(1,q1[0]);
+            q1.shift();
+            if(q1.length == 0)
+                queue.shift();
+        }
+        else if(queue[0] == 2)
+        {
+            console.log(2,q2[0]);
+            q2.shift();
+            if(q2.length == 0)
+                queue.shift();
+        }
+        else if(queue[0] == 3)
+        {
+            console.log(3,q3[0]);
+            q3.shift();
+            if(q3.length == 0)
+                queue.shift();
+        }
+        else
+        {
+            console.log(4,q4[0]);
+            q4.shift();
+            if(q4.length == 0)
+                queue.shift();
+        }
+    }
+}
--- Masai/DSA-Contest-3.3.3.2/Unique-Gift.js
+++ Masai/DSA-Contest-3.3.3.2/Unique-Gift.js
@@ -4,34 +4,46 @@ https://oj.masaischool.com/contest/2938/problem/02
 
 function uniqueGift(str)
 {
+    //stores unique elements
     var queue = [];
-    var bag = "";
-    var front = 0;
+
+    //to keep track of elements counts
+    var map = {};
+
+    //final answer 
+    var unique = [];
+
     for(let i = 0; i < str.length; i++)
     {
-        if(queue.includes(str[i]))
-        {
-            if(queue[front] == str[i])
-                front++;
-            else
-            {
-                var index = queue.indexOf(str[i]);
-                if (index !== -1) 
-                    queue.splice(index, 1);
-            }
-            if(queue[front] == undefined)
-                bag += "#";
-            else
-                bag += queue[front];
-        }
-            
-        else
+        //enqueue the first gift
+        if(i == 0)
         {
             queue.push(str[i]);
-            bag += queue[front];
-        }      
+            map[str[i]] = 1;
+        }
+        else
+        {
+            //if the gift is unique
+            if(map[str[i]] == undefined)
+            {
+                queue.push(str[i]);
+                map[str[i]] = 1;
+            }
+
+            //if the gift is not unique
+            else
+            {
+                //update gift map
+                map[str[i]] = map[str[i]] + 1;
+
+                //dequeue until the first element in the queue is unique
+                while(map[queue[0]] > 1)
+                    queue.shift();
+            }
+        }
+        unique.push(queue[0] || "#");
     }
-    console.log(bag);
+    console.log(unique.join(""));
 
 }
 
@@ -39,4 +51,3 @@ function uniqueGift(str)
 //Sample Input
 uniqueGift("abadbc");
 uniqueGift("abcabc");
-uniqueGift("wnryhycoynfetlqeziwq");
